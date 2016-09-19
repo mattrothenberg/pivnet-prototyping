331940c5bd818c1689eff05f4ab0ece034233b5a
Matt Rothenberg <mattrothenberg@gmail.com>
2016-09-19 10:55:07 -0400

    Improve UX of search popup by autofocusing input

--- app/scripts/directives/escapeclose.js
+++ app/scripts/directives/escapeclose.js
@@ -7,34 +7,45 @@
  * # escapeClose
  */
 angular.module('labsDesignYeoman')
-  .directive('escapeClose', function() {
+  .directive('escapeClose', function($timeout) {
     return {
       restrict: 'EAC',
       link: function(scope, element, attrs) {
+        init();
 
-        var body = document.querySelectorAll('body')[0];
-        body.classList.toggle('has-modal-open');
 
-        function escapeToClose(event) {
-          if (event.keyCode == 27) {
+        function init() {
+          // Prevent body from scrolling underneath modal
+          var body = document.querySelectorAll('body')[0];
+          body.classList.toggle('has-modal-open');
+
+          // Focus the autocomplete input
+          $timeout(function() {
+            var input = document.getElementById('modalProductSearch').childNodes[0].childNodes[1];
+            input.focus();
+          })
+
+          var backdrop = document.querySelectorAll('.full-screen-search-overlay')[0];
+          backdrop.addEventListener('click', function() {
             scope.$apply(function() {
               scope.$parent.fullScreenSearchShowing = false;
             })
-          }
-        }
-
-        var backdrop = document.querySelectorAll('.full-screen-search-overlay')[0];
-        backdrop.addEventListener('click', function() {
-          scope.$apply(function() {
-            scope.$parent.fullScreenSearchShowing = false;
           })
-        })
 
-        document.addEventListener('keydown', escapeToClose);
-        scope.$on('$destroy', function() {
-          body.classList.toggle('has-modal-open');
-          document.removeEventListener('keydown', escapeToClose)
-        })
+          function escapeToClose(event) {
+            if (event.keyCode == 27) {
+              scope.$apply(function() {
+                scope.$parent.fullScreenSearchShowing = false;
+              })
+            }
+          }
+
+          document.addEventListener('keydown', escapeToClose);
+          scope.$on('$destroy', function() {
+            body.classList.toggle('has-modal-open');
+            document.removeEventListener('keydown', escapeToClose)
+          })
+        }
       }
     }
   });
